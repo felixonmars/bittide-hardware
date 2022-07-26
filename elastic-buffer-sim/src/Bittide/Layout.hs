8287ddd39966448788869de8b596b2609243aa27
Vanessa McHale <vamchale@gmail.com>
2022-07-26 10:52:46 -0500

    exe to dump with ps times

--- elastic-buffer-sim/src/Bittide/Layout.hs
+++ elastic-buffer-sim/src/Bittide/Layout.hs
@@ -1,8 +1,11 @@
 -- | This module contains static topologies.
-module Bittide.Layout ( genOffs, twoNodes, threeNodes ) where
+module Bittide.Layout ( dumpCsv, genOffs, twoNodes, threeNodes ) where
 
 import Clash.Explicit.Prelude
 import Numeric.Natural
+import qualified Prelude as P
+
+import Clash.Signal.Internal (Signal (..))
 
 import qualified Data.ByteString.Lazy as BSL
 import Data.Csv
@@ -11,17 +14,27 @@ import System.Random (randomRIO)
 import Arithmetic
 import Bittide.Simulate
 
+type Ps = Natural
+
+timeClock :: Signal dom (Natural, a, b) -> [(Ps, Natural, a, b)]
+timeClock = go 0
+ where
+  go t ((ᴛ, x, y) :- xs) = (t, ᴛ, x, y) : go (t+ᴛ) xs
+
 -- | This can be used inside a REPL and fed to @script.py@
-dumpCsv :: IO ()
-dumpCsv = do
+dumpCsv :: Int -> IO ()
+dumpCsv n = do
   o1 <- genOffs
   o2 <- genOffs
   o3 <- genOffs
-  writeFile "clocks.csv" "clk0,eb01,eb02,,clk1,eb10,eb12,,clk2,eb20,eb21,,\n"
+  writeFile "clocks.csv" "t,clk0,eb01,eb02,,\n" -- clk1,eb10,eb12,,clk2,eb20,eb21,,\n"
+  -- FIXME: sample on a ps-basis?
   BSL.appendFile "clocks.csv"
     $ encode
-    $ sampleN 10000000
-    $ bundle
+    $ P.take n
+    -- $ (\(a, b, c) -> P.zipWith3 (\(t,x0,y0,z0) (_,x1,y1,z1) (_,x2,y2,z2) -> (t,x0,y0,z0)) a b c)
+
+    $ (\(c0, e00, e01, c1, c10, c11, c2, c20, c21) -> timeClock (bundle (c0, e00, e01))) -- , timeClock (bundle (c1, c10, c11)), timeClock (bundle (c2, c20, c21))))
     $ threeNodes @Bittide @Bittide @Bittide o1 o2 o3
 
 genOffs :: IO Offset
@@ -85,29 +98,29 @@ threeNodes ::
   ( Signal dom1 Natural
   , Signal dom1 Natural
   , Signal dom1 Natural
-  , Signal dom1 SpeedChange
+  -- , Signal dom1 SpeedChange
   , Signal dom2 Natural
   , Signal dom2 Natural
   , Signal dom2 Natural
-  , Signal dom2 SpeedChange
+  -- , Signal dom2 SpeedChange
   , Signal dom3 Natural
   , Signal dom3 Natural
   , Signal dom3 Natural
-  , Signal dom3 SpeedChange
+  -- , Signal dom3 SpeedChange
   )
 threeNodes offs0 offs1 offs2 =
   ( clk0Signal
   , eb01
   , eb02
-  , clockControl0
+  -- , clockControl0
   , clk1Signal
   , eb10
   , eb12
-  , clockControl1
+  -- , clockControl1
   , clk2Signal
   , eb20
   , eb21
-  , clockControl2
+  -- , clockControl2
   )
  where
 
