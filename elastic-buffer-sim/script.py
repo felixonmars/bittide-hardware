5b88b02fd3cd8b3d7a0251d1b9f77b90ae5ae833
Vanessa McHale <vamchale@gmail.com>
2022-08-01 12:38:10 -0400

    x-axis time
    more sensible concatenation; plot still works

--- elastic-buffer-sim/script.py
+++ elastic-buffer-sim/script.py
@@ -6,6 +6,10 @@ df0=pd.read_csv('clocks0.csv')
 df1=pd.read_csv('clocks1.csv')
 df2=pd.read_csv('clocks2.csv')
 
+df0.set_index('t', inplace=True)
+df1.set_index('t', inplace=True)
+df2.set_index('t', inplace=True)
+
 df=pd.concat([df0,df1,df2],axis=0)
 
 df[['eb01','eb02','eb10','eb12','eb20','eb21']].plot()
