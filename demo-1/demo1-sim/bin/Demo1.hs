64d4aa2d59390c9978300d9b02795f82683e0d3e
Lara Herzog <lara@qbaylogic.com>
2022-09-23 15:51:26 +0200

    write ping-pong programs

--- demo-1/demo1-sim/bin/Demo1.hs
+++ demo-1/demo1-sim/bin/Demo1.hs
@@ -40,10 +40,41 @@ nodeCircuit
 nodeCircuit clk rst (unbundle -> inLinks) =
   let
     gppeMemMap :: Vec 2 (BitVector 32) -> Vec 4 (BitVector 32)
-    gppeMemMap (iMem :> dMem :> Nil) = iMem :> dMem :> dMem + 234324 :> dMem + 3434343 :> Nil
+    gppeMemMap (iMem :> dMem :> Nil) =
+      iMem :>
+      dMem :>
+      0x00010000 :> -- scatter memory
+      0x00018000 :> -- gather memory
+      Nil
 
     managementMemMap :: Vec 2 (BitVector 32) -> Vec 25 (BitVector 32)
-    managementMemMap (iMem :> dMem :> Nil) = iMem :> dMem :> fmap (+32768) (iterateI succ dMem)
+    managementMemMap (iMem :> dMem :> Nil) =
+      iMem :>
+      dMem :>
+      0x00010000 :> -- scatter memory
+      0x00018000 :> -- gather memory
+      0x00020000 :> -- rx Unit
+      0x00020400 :> -- Scatter unit calendar
+      0x00022400 :> -- tx Unit
+      0x00022800 :> -- Gather unit calendar
+      0x00030000 :> -- Switch calendar
+      0x00032000 :> -- Link 0 rx Unit
+      0x00032400 :> -- Link 0 tx Unit
+      0x00040000 :> -- GPPE 0 rx Unit
+      0x00040400 :> -- GPPE 0 Scatter unit calendar
+      0x00042400 :> -- GPPE 0 tx Unit
+      0x00042800 :> -- GPPE 0 Gather unit calendar
+      0x00050000 :> -- GPPE 1 rx Unit
+      0x00050400 :> -- GPPE 1 Scatter unit calendar
+      0x00052400 :> -- GPPE 1 tx Unit
+      0x00052800 :> -- GPPE 1 Gather unit calendar
+      0x10000000 :> -- ???
+      0x11000000 :> -- ???
+      0x12000000 :> -- ???
+      0x13000000 :> -- ???
+      0x14000000 :> -- ???
+      0x15000000 :> -- ???
+      Nil
 
 
     pingPeConfig = peConfigFromElfConfig gppeMemMap $(processElfFile "ping" "gppe")
